3b22566cc7461115b588edf26830c7f401110e33
Rafael de Macedo Corrêa <rafael.correa82@gmail.com>
2014-05-23 19:13:14 -0300

    Initial Commit

---
+++ primeiro/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  primeiro
+//
+//  Created by Rafael M Corrêa on 23/05/14.
+//  Copyright (c) 2014 Rafael Corrêa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
